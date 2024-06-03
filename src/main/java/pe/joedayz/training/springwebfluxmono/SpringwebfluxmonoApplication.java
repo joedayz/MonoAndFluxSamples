d9c9ecff0d49255249af135738976471c023b57c
Jose Diaz <jose@exoreaction.com>
2024-06-02 21:27:21 -0500

    rest api con webflux

--- src/main/java/pe/joedayz/training/springwebfluxmono/SpringwebfluxmonoApplication.java
+++ src/main/java/pe/joedayz/training/springwebfluxmono/SpringwebfluxmonoApplication.java
@@ -2,7 +2,9 @@ package pe.joedayz.training.springwebfluxmono;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+@EnableReactiveMongoRepositories
 @SpringBootApplication
 public class SpringwebfluxmonoApplication {
 
